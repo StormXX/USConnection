2e9d71fcf87b622a34e326071311ef412b7cfd9b
Gintama <gintama@gintamadembp.lan>
2014-05-04 19:14:41 +0800

    Initial Commit

---
+++ USConnection/TAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TAppDelegate.h
+//  USConnection
+//
+//  Created by Gintama on 14-5-4.
+//  Copyright (c) 2014年 Gintama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ USConnection/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  USConnection
+//
+//  Created by Gintama on 14-5-4.
+//  Copyright (c) 2014年 Gintama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TAppDelegate class]));
+    }
+}
